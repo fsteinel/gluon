86ed5d3e74baeea245b5a47ba8b35ee5f5873b72
Christof <christof.schulze@gmx.net>
2016-11-10 14:57:14 +0100

    remove default wan firewall zone and replace it with a gluon-mesh-babel-based wan zone
    add logging to firewall for now
    
    Bug-squashing:
    * disable igmp snooping on br-client for babel networks
    
    * gluon-mesh-babel provides the information on which device respondd should be running besides mesh-interfaces
    * rework l3roamd initialization: remove hardcoded mesh-interfaces
    
    rework mmfd-initialization
    * mmfd will only be started after a successful connection to babelds
      socket was made.
    * For establishing an order of the setup.d scripts by adding a number
    * the mmfd0-interface will be set up having a generic fe80-IP-Address
    * Add rules to firewall to allow inbound mmfd-traffic on mmfd zone and
      on mmfd-interface
    
    use network name instead of raw interface name

--- package/gluon-mesh-babel/files/lib/gluon/core/mesh/teardown.d/babel-del-interface.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-PORT=33123
-if [ $(uci get network.$CONFIG.proto) == gluon_mesh ]
-then                                                 
-  echo flush interface $IFNAME| telnet ::1 33123         
-  grep -v "fw-addif $IFNAME" /tmp/liverules.iptables|sort -u >/tmp/liverules.iptables.$$
-  mv /tmp/liverules.iptables.$$ /tmp/liverules.iptables
-fi
